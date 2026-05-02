a6ceba583db3366cbec89186570ec7a49dd9ad60
hicham <relaxingchannel65@gmail.com>
2026-05-02 11:46:38 +0100

    first commit

--- components/MainSection/NavBar.jsx
+++ components/MainSection/NavBar.jsx
@@ -28,7 +28,7 @@ export default function Component() {
       <div className="flex h-16 items-center justify-between gap-4">
         {/* Left side  */}
         <div className="flex items-center gap-2 flex-1">
-          {/* Mobile menu trigger */}
+          {/* Mobile menu triggerrrr */}
           <Popover>
             <PopoverTrigger asChild>
               <Button
